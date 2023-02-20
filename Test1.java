453ed26377b9414b88e886212d33f9cf88268ca4
Jasbac0002 <125256049+Jasbac0002@users.noreply.github.com>
2023-02-20 15:44:58 +1300

    Update Test1.java
    Change to Loop

--- Test1.java
+++ Test1.java
@@ -1,9 +1,9 @@
-public class test
-
+public class Test
 {
-	public static void main(String[] args){
-		System.out.println("Hello,World!");
+	public static void main(String[] args)
+	{
+	int tableNo=0;
+	for(int i=1; i <=; i++)
+		System.out.println(tableNo + " * " + i + " = " + tableNo *i);
 	}
-
-
 }
